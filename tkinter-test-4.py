80d76e555c7c148fb8eb13a421f629735290e6ec
Elizabeth Tweedale <elizabeth@cyphercoding.com>
2017-03-05 16:45:00 +0000

    update colour

--- tkinter-test-4.py
+++ tkinter-test-4.py
@@ -19,7 +19,7 @@ class Example(Frame):
         canvas.create_rectangle(30, 10, 120, 80, 
             outline="#fb0", fill="#fb0")
         canvas.create_rectangle(150, 10, 240, 80, 
-            outline="#f50", fill="#f50")
+            outline="#f50", fill="#f51")
         canvas.create_rectangle(270, 10, 370, 80, 
             outline="#05f", fill="#05f")            
         canvas.pack(fill=BOTH, expand=1)
